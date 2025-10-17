965ace9f18c56d2ccf92dfefb40c046dbe46c9f2
Nick <151399981+L0af1@users.noreply.github.com>
2025-10-17 15:31:58 +0100

    First commit
    Added Files. That will help me build this website.

---
+++ Tutoring/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <h2 class="logo">TutorConnect</h2>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/tutors">Tutors</router-link></li>
+        <li><router-link to="/login">Login</router-link></li>
+        <li><router-link to="/signup">Signup</router-link></li>
+        <li><router-link to="/cart">Bookings</router-link></li>
+      </ul>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
