670ac87cc4d6834972009cb818a99131d37fe7d2
Nick <151399981+L0af1@users.noreply.github.com>
2025-10-20 12:29:53 +0100

    Installed the Vue-Router 4. Done the Logic for index.js
    Pathing doesnt work at the moment because of the Login and Signup. But once i write the code for them it should all work. Should of installed the Vue Router at the start but its okay.

---
+++ Tutoring/src/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from './Home.vue'
+import Tutors from './Tutors.vue'
+import TutorProfile from './TutorProfile.vue'
+import Login from './Login.vue' // soon ill add this, just need to write the cpde
+import Signup from './Signup.vue' // soon ill add this, just need to write the code
+import Cart from './Cart.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/tutors', component: Tutors },
+  { path: '/tutor/:id', component: TutorProfile, props: true },
+  { path: '/login', component: Login }, // soon ill add this, just need to write the code. (Pathing)
+  { path: '/signup', component: Signup }, // soon ill add this, just need to write the code. (Pathing)
+  { path: '/cart', component: Cart },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+export default router
